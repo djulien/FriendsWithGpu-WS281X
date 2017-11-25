50e21ee661d39768a9d5c21121d62709990fee02
dj <djulien17@thejuliens.net>
2017-11-25 14:48:54 -0800

    fix symlink

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,16 +9,27 @@ MY_TOP=`git rev-parse --show-toplevel`  #from https://unix.stackexchange.com/que
 #source  "$MY_TOP"/scripts/colors.sh
 source  "$HERE"/colors.sh
 
+relpath()
+{
+    realpath --relative-to="$1" "$2"
+}
+
+DEPS="libsdl2-dev"
+TARGET="$MY_TOP/node_modules/gpu-friends-ws281x"
+echo "here $HERE"
+echo `relpath $MY_TOP, $TARGET`
+
 if [ "x$1" == "xpre" ]; then
   echo -e  "${CYAN}installing dependencies ...${NORMAL}"
-  sudo  apt-get install  libsdl2-dev
+  sudo  apt-get install  "$DEPS"
   echo -e  "${GREEN}dependencies installed.${NORMAL}"
 elif [ "x$1" == "xpost" ]; then
-  if [ -d "$MY_TOP/node_modules/gpu-friends-ws281x" ]; then
+  if [ -d "$TARGET" ]; then
     echo -e  "${GREEN}no symbolic link needed.${NORMAL}"
   else
-    echo -e  "${CYAN}making symbolic links ...${NORMAL}"
-    ln -snf $MY_TOP/node_modules/gpu-friends-ws281x
+    echo -e  "${CYAN}making symbolic link ...${NORMAL}"
+    ln -s  "$MY_TOP"  "`relpath $MY_TOP, $TARGET`"
+#    ln -snf "$HERE/.." "$TARGET"
     echo -e  "${GREEN}symbolic links created.${NORMAL}"
   fi
 else
