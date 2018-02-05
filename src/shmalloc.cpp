92d8f1596b24749a44baf37d98a89c6b8959e87a
djulien <djulien17@thejuliens.net>
2018-02-05 12:06:45 -0800

    start addibg shm allocator

--- src/shmalloc.cpp
+++ src/shmalloc.cpp
@@ -2,9 +2,9 @@
 //to compile:
 // g++  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fexceptions  -w -Wall -pedantic -Wvariadic-macros -g -std=c++11 shmalloc.cpp -o shmalloc
 
+
 //see https://www.ibm.com/developerworks/aix/tutorials/au-memorymanager/index.html
 //see also http://anki3d.org/cpp-allocators-for-games/
-
 
 //custom stream manipulator for printf-style formating:
 //idea from https://stackoverflow.com/questions/535444/custom-manipulator-for-c-iostream
@@ -70,17 +70,141 @@ std::string timestamp()
 }
 
 
+//atomic operations:
+//std::recursive_mutex atomic_mut;
+#include <mutex>
+std::mutex atomic_mut;
+//use macro so stmt can be nested within scoped lock:
+#define ATOMIC(stmt)  { std::unique_lock<std::mutex> lock(atomic_mut); stmt; }
+
+
 ////////////////////////////////////////////////////////////////////////////////
 
+
+// https://stackoverflow.com/questions/5656530/how-to-use-shared-memory-with-linux-in-c
+// https://stackoverflow.com/questions/4836863/shared-memory-or-mmap-linux-c-c-ipc
+
+//    you identify your shared memory segment with some kind of symbolic name, something like "/myRegion"
+//    with shm_open you open a file descriptor on that region
+//    with ftruncate you enlarge the segment to the size you need
+//    with mmap you map it into your address space
+
+//template<int SIZE>
+#include <iostream>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <errno.h>
+#include <cstdlib>
+#include <string.h>
+class ShmHeap
+{
+public:
+    ShmHeap(int key): ShmHeap(key, 0, false) {} //children
+    ShmHeap(int key, size_t size, bool cre): m_key(cre? key: 0), m_ptr(0), m_size(0)
+    {
+//        if ((key = ftok("hello.txt", 'R')) == -1) /*Here the file must exist */ 
+//        int fd = shm_open(filepath, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+//        if (fd == -1) throw "shm_open failed";
+//        if (ftruncate(fd, sizeof(struct region)) == -1)    /* Handle error */;
+//        rptr = mmap(NULL, sizeof(struct region), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+//        if (rptr == MAP_FAILED) /* Handle error */;
+//create shm seg:
+//        if (!key) throw "ShmHeap: bad key";
+        if (!key) key = (rand() << 16) | 0xfeed;
+        if (cre) destroy(); //delete first (parent wants clean start)
+    	if ((size < 1) || (size >= 10000000)) throw "ShmHeap: bad size"; //set reasonable limits
+//NOTE: size will be rounded up to a multiple of PAGE_SIZE
+        int shmid = shmget(key, size, (cre? IPC_CREAT | IPC_EXCL: 0) | 0666); // | SHM_NORESERVE); //NOTE: clears to 0 upon creation
+        if (shmid == -1) throw strerror(errno); //failed to create or attach
+        void* ptr = shmat(shmid, NULL /*system choses adrs*/, 0); //read/write access
+        if (ptr == (void*)-1) throw strerror(errno);
+        m_key = cre? key: 0;
+        m_ptr = ptr;
+        m_size = size;
+        ATOMIC(std::cout << "ShmHeap: attached " << size << " bytes at " << FMT("0x%x") << m_ptr << ", used = " << used() << ", avail " << available() << "\n" << std::flush);
+    }
+    ~ShmHeap() { detach(); destroy(); }
+public:
+    inline int key() { return m_key; }
+    inline size_t size() { return m_size; }
+    inline size_t used() { return *(size_t*)m_ptr + sizeof(size_t); } //account for used space at first location
+    inline size_t available() { return size() - used(); }
+    inline const char* symtab() { return m_ptr + sizeof(size_t); }
+public:
+//generate a unique key from a src location:
+//same src location will generate same key, even across processes
+    static void crekey(std::string& key, const char* filename, int line)
+    {
+        const char* bp = strrchr(filename, '/');
+        if (!bp) bp = filename;
+        key = bp;
+        key += ':';
+        key += line;
+    }
+    void* alloc(const char* key, int size, int alignment = 4) //assume 32-bit alignment, allow override; https://en.wikipedia.org/wiki/LP64
+    {
+//more info about alignment: https://en.wikipedia.org/wiki/Data_structure_alignment
+//first check if symbol already exists:
+>>>>>>>>>>>>
+        const char* bp = m_ptr + 
+        int keylen = rdup(strlen(key) + 1, alignment);
+        size = rdup(size, alignment);
+        if (available() < keylen + size) return 0; //not enough space
+        void* nextptr = m_ptr + used();
+        strcp
+    }
+    void destroy() //int key)
+    {
+        if (!m_key) return; //not owner
+        int shmid = shmget(m_key, 1, 0666); //use minimum size in case it changed
+        m_key = 0;
+        if ((shmid != -1) && !shmctl(shmid, IPC_RMID, NULL /*ignored*/)) return; //successfully deleted
+        if ((shmid == -1) && (errno == ENOENT)) return; //didn't exist
+        throw strerror(errno);
+    }
+    void detach()
+    {
+        if (!m_ptr) return;
+        if (shmdt(m_ptr) == -1) throw strerror(errno);
+        ATOMIC(std::cout << "ShmHeap: dettached " << m_size << " bytes from " << FMT("0x%x") << m_ptr << "\n" << std::flush);
+        m_ptr = 0;
+    }
+private:
+    int m_key; //only used for owner
+    void* m_ptr; //ptr to start of shm
+    size_t m_size; //total size (bytes)
+>>>>>>>>>>>
+    struct { size_t used, nextptr; char* name; } hdr;
+};
+ShmHeap shmheap(0, 0x1000, true);
+
+
+void* operator new(size_t size, const char* filename, int line)
+{
+    void* ptr = new char[size];
+//    ++Complex::nalloc;
+//    if (Complex::nalloc == 1)
+        std::cout << "size = " << size << " filename = " << filename << " line = " << line << "\n" << std::flush;
+    return ptr;
+}
+//tag alloc with src location:
+//NOTE: cpp avoids recursion so macro names can match actual function names here
+#define new  new(__FILE__, __LINE__)
+
+
 //overload new + delete operators:
-//NOTE: these act like static members (no "this")
+//can be global or class-specific
+//NOTE: these are static members (no "this")
 //void* operator new(size_t size); 
-//void   operator delete(void* pointerToDelete); 
+//void operator delete(void* pointerToDelete); 
 //-OR-
 //void* operator new(size_t size, MemoryManager& memMgr); 
-//void   operator delete(void* pointerToDelete, MemoryManager& memMgr);
+//void operator delete(void* pointerToDelete, MemoryManager& memMgr);
 
+// https://www.geeksforgeeks.org/overloading-new-delete-operator-c/
+// https://stackoverflow.com/questions/583003/overloading-new-delete
 
+#if 0
 class IMemoryManager 
 {
 public:
@@ -95,8 +219,6 @@ public:
     virtual void* allocate(size_t);
     virtual void free(void*);
 };
-
-
 MemoryManager gMemoryManager; // Memory Manager, global variable
 void* operator new(size_t size)
 {
@@ -114,20 +236,61 @@ void operator delete[](void* arrayToDelete)
 {
     gMemoryManager.free(arrayToDelete);
 }
-
+#endif
 
 
 class Complex 
 {
 public:
+    Complex() {} //needed for custom new()
     Complex (double a, double b): r (a), c (b) {}
 private:
     double r; // Real Part
     double c; // Complex Part
+public:
+#if 0 //custom new + delete
+    void* operator new(size_t size)
+    {
+        void* ptr = ::new Complex(); //CAUTION: force global new() here (else recursion)
+//        void* ptr = malloc(size); //alternate
+        ++nalloc;
+        return ptr;
+    }
+    void operator delete(void* ptr)
+    {
+        ++nfree;
+        free(ptr);
+    }
+    void* operator new (size_t size, const char* filename, int line)
+    {
+        void* ptr = new char[size];
+        cout << "size = " << size << " filename = " << filename << " line = " << line << endl;
+        return ptr;
+    }
+#endif
+    static int nalloc, nfree;
 };
+int Complex::nalloc = 0;
+int Complex::nfree = 0;
 
 
+#if 0
 #include <iostream>
+
+void* operator new(size_t size, const char* filename, int line)
+{
+    void* ptr = new char[size];
+    ++Complex::nalloc;
+    if (Complex::nalloc == 1)
+        std::cout << "size = " << size << " filename = " << filename << " line = " << line << "\n" << std::flush;
+    return ptr;
+}
+//tag alloc with src location:
+//NOTE: cpp avoids recursion so macro names can match actual function names here
+#define new  new(__FILE__, __LINE__)
+#endif
+
+
 
 int main(int argc, char* argv[]) 
 {
@@ -141,6 +304,7 @@ int main(int argc, char* argv[])
             delete array[j];
     }
     std::cout << timestamp() << "finish\n" << std::flush;
+    std::cout << "#alloc: " << Complex::nalloc << ", #free: " << Complex::nfree << "\n" << std::flush;
     return 0;
 }
 
